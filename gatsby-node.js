1df53e1a14054ca9a2387d290dbdcf4c211b68cc
naeluh <naeluh@gmail.com>
2020-01-02 16:16:41 -0800

    add dynamic page rendering

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,57 @@
  */
 
 // You can delete this file if you're not using it
+const path = require(`path`)
+
+// Create a slug for each article and set it as a field on the node.
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `node__article`) {
+    const slug = `/article/${node.drupal_internal__nid}/`
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const articleTemplate = path.resolve(`src/templates/article.js`)
+  // Query for article nodes to use in creating pages.
+  return graphql(
+    `
+      {
+        articles: allNodeArticle {
+          edges {
+            node {
+              internalId: drupal_internal__nid
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // Create pages for each recipe.
+    result.data.articles.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: articleTemplate,
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
+}
---
+++ src/templates/article.js
@@ -0,0 +1,14 @@
+import { graphql } from "gatsby"
+import React from "react"
+import Img from "gatsby-image"
+
+const ArticleTemplate = ({ data }) => <h1>{data.article.title}</h1>
+
+export default ArticleTemplate
+
+export const query = graphql`
+query($slug: String!) {
+    article: nodeArticle(fields: { slug: { eq: $slug } }) {
+      title
+    }
+`
