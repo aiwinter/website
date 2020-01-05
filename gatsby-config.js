fbb842d03ae6003dfd38d84a7ab9603f9b5388af
naeluh <naeluh@gmail.com>
2020-01-04 16:27:47 -0800

    update name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `AI Winter`,
+    title: `A. I. Winter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
