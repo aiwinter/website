95a17fd9b9917ff8756d1785a389188183a23e2a
naeluh <naeluh@gmail.com>
2020-05-17 12:42:28 -0700

    Update default manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,8 +24,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ai-winter`,
+        short_name: `aiwinter`,
         start_url: `/`,
         background_color: `#000`,
         theme_color: `#000`,
